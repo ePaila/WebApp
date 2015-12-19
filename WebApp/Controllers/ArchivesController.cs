cfbaa6a73322625a6f104d733dfd080b9a4e8f88
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2015-12-19 14:50:11 -0800

    Push new changes to master

---
+++ WebApp/Controllers/ArchivesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ePaila.ViewModel;
+using ePaila.Data.Repo;
+
+namespace WebApp.Controllers
+{
+    public class ArchivesController : BaseController
+    {
+        ArticleRepository _articleRepo;
+        public ArchivesController(ePaila.Data.ePailaEntities db) : base(db)
+        {
+            _articleRepo = new ArticleRepository(db);
+        }
+
+        // GET: Archives
+        public ActionResult Index()
+        {
+            ArchivesViewModel model = new ArchivesViewModel();
+            model.Articles = _articleRepo.GetAll();
+            return View(model);
+        }
+    }
+}
