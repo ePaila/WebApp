70f7cf2df98f2c35bc4e13df671b20eb63172820
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2015-12-27 19:59:37 -0800

    new page eTV added About Me page changed

---
+++ ePaila.Model/eTVViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePaila.ViewModel
+{
+    public class eTVViewModel:LeftPanelViewModel
+    {
+    }
+}
