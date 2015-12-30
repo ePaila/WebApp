c309131cf687303fd7495618aaabcbdfa6ae00e9
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2015-12-29 19:57:16 -0800

    added flip card on news box left panel changes

---
+++ ePaila.Utility/eXML.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ePaila.Utility
+{
+    public class eXML
+    {
+        /// <summary>
+        /// Parse Un Formatted XML to Fromatted XML String
+        /// </summary>
+        /// <param name="strXML"></param>
+        /// <returns></returns>
+        public static string Parse(string strXML)
+        {
+            //trim first line space
+            
+            strXML.Trim();
+            string formattedXml = XElement.Parse(strXML).ToString();
+            return formattedXml;
+        }
+    }
+}
--- news.ePaila.com/Controllers/HomeController.cs
+++ news.ePaila.com/Controllers/HomeController.cs
@@ -18,15 +18,41 @@ namespace news.ePaila.com.Controllers
             FeedChannel channel_01 = new FeedChannel()
             {
                 Title = "Setopati",
-                URL = "http://setopati.com/",
+                URL = "http://www.setopati.com/",
                 FeedURL = "http://setopati.com/rss/"
             };
 
+            FeedChannel channel_02 = new FeedChannel()
+            {
+                Title = "Onlinekhabar",
+                URL = "http://www.onlinekhabar.com/",
+                FeedURL = "http://www.onlinekhabar.com/feed/"
+            };
+            FeedChannel channel_03 = new FeedChannel()
+            {
+                Title = "Ratopati",
+                URL = "http://ratopati.com/",
+                FeedURL = "http://www.ratopati.com/feed/"
+            };
+
+            FeedChannel channel_04 = new FeedChannel()
+            {
+                Title = "Pahilopost",
+                URL = "http://pahilopost.com/",
+                FeedURL = "http://www.pahilopost.com/rss/"
+            };
+            
             model.Channels.Add(channel_01);
+            model.Channels.Add(channel_02);
+            model.Channels.Add(channel_03);
+            model.Channels.Add(channel_04);
 
             //load rss items
             NewsFeedRadarRepository feedRepo = new NewsFeedRadarRepository();
-            model.Items = feedRepo.GetFeedItems(channel_01);
+            foreach (var item in model.Channels)
+            {
+                model.Items.AddRange(feedRepo.GetFeedItems(item));
+            }
             return View(model);
         }
     }
