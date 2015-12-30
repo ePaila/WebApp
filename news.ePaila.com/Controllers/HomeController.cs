e9fe94eca5592fbd1f0a4e31d1c4ea3a017ace60
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2015-12-29 23:30:17 -0800

    news.epaila.com version 1.0 published

--- news.ePaila.com/Controllers/HomeController.cs
+++ news.ePaila.com/Controllers/HomeController.cs
@@ -15,38 +15,72 @@ namespace news.ePaila.com.Controllers
         {
             FeedMeViewModel model = new FeedMeViewModel();
             //add channel
-            FeedChannel channel_01 = new FeedChannel()
+            #region Add Channel
+            FeedChannel channel;
+            channel = new FeedChannel()
             {
                 Title = "Setopati",
                 URL = "http://www.setopati.com/",
                 FeedURL = "http://setopati.com/rss/"
             };
+            model.Channels.Add(channel);
 
-            FeedChannel channel_02 = new FeedChannel()
+            channel = new FeedChannel()
             {
                 Title = "Onlinekhabar",
                 URL = "http://www.onlinekhabar.com/",
                 FeedURL = "http://www.onlinekhabar.com/feed/"
             };
-            FeedChannel channel_03 = new FeedChannel()
+            model.Channels.Add(channel);
+
+            channel = new FeedChannel()
             {
                 Title = "Ratopati",
                 URL = "http://ratopati.com/",
-                FeedURL = "http://www.ratopati.com/feed/"
+                FeedURL = "http://www.ratopati.com/newsfeed/"
             };
+            model.Channels.Add(channel);
 
-            FeedChannel channel_04 = new FeedChannel()
+            channel = new FeedChannel()
             {
                 Title = "Pahilopost",
                 URL = "http://pahilopost.com/",
                 FeedURL = "http://www.pahilopost.com/rss/"
             };
-            
-            model.Channels.Add(channel_01);
-            model.Channels.Add(channel_02);
-            model.Channels.Add(channel_03);
-            model.Channels.Add(channel_04);
+            model.Channels.Add(channel);
 
+            channel = new FeedChannel()
+            {
+                Title = "Bizmandu",
+                URL = "http://bizmandu.com/",
+                FeedURL = "http://www.bizmandu.com/feed/news"
+            };
+            model.Channels.Add(channel);
+
+            channel = new FeedChannel()
+            {
+                Title = "Imagekhabar",
+                URL = "http://imagekhabar.com/",
+                FeedURL = "http://www.imagekhabar.com/feed/"
+            };
+            model.Channels.Add(channel);
+
+            channel = new FeedChannel()
+            {
+                Title = "Himalkhabar",
+                URL = "http://himalkhabar.com/",
+                FeedURL = "http://www.himalkhabar.com/feed"
+            };
+            model.Channels.Add(channel);
+
+            channel = new FeedChannel()
+            {
+                Title = "Ujyaaloonline",
+                URL = "http://ujyaaloonline.com/",
+                FeedURL = "http://ujyaaloonline.com/rss"
+            };
+            model.Channels.Add(channel);    
+            #endregion
             //load rss items
             NewsFeedRadarRepository feedRepo = new NewsFeedRadarRepository();
             foreach (var item in model.Channels)
